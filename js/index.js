7fb9dca1ca63cd76bace988032514c9cccc00dcd
MrChock <student10@js-c02kp2dlf5n7.punahou.edu>
2016-06-27 11:56:15 -1000

    We started writing java

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var	arr = ['red' , 'orange' , 'yellow' 'green' 'blue' 'indigo' 'violet']; 
+
+var count = 0; 
+
+var cycle = function (array){
+	console.log(array)[count]
+	if (count >= array.length);
+	count ++;
+	var list = function(){ cycle(arr);}
+
+	])
+
+	}
